7310f0757f82ec211bb316e548bb02ebcf7f1b3e
Juan <juancurti.it@gmail.com>
2021-01-22 00:35:46 +0100

    Complete Affiliation Contest

--- components/gamblePopup.js
+++ components/gamblePopup.js
@@ -135,7 +135,7 @@ export default class WithdrawPopup extends React.Component {
             </div>
             <PaginatedList
               list={this.state.history}
-              itemsPerPage={3}
+              itemsPerPage={2}
               renderList={(list) => (
                 <table className='bp-table wallet-table predictTable gambleT'>
  
